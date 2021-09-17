46966590cbff1f11fb2faf7b8c42f7b1385e9734
aleks <aleks.mihaylov69@gmail.com>
2021-09-17 17:33:44 +0300

    uninstall ramda

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,16 @@
 import "../scss/app.scss";
-const R = require("ramda");
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
 
   const ul = document.querySelector("ul");
   const numbersArray = [1, 3, 4, 5, 6, 7];
   const isEven = (n) => n % 2 === 0;
-  const filteredArray = R.filter(isEven, numbersArray);
+  // const filteredArray = R.filter(isEven, numbersArray);
+  const filteredArray = numbersArray.filter((el) => isEven(el));
+
+  filteredArray.forEach((el) => {
+    const li = document.createElement("li");
+    li.textContent = el;
+    ul.appendChild(li);
+  });
 });
